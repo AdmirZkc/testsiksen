c095b1dbc159d1881dde850ff1f24e842e152f66
AdmirZkc <wi21b028@technikum-wien.at>
2022-06-21 16:52:30 +0200

    initial commit

---
+++ src/main/java/at/technikum/testsiksen/TestsiksenApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.testsiksen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsiksenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsiksenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/testsiksen/TestsiksenApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.testsiksen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsiksenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
