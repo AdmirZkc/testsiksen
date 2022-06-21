4c85f11154b88797db77fa608b66191eb31ddca0
AdmirZkc <wi21b028@technikum-wien.at>
2022-06-21 20:17:15 +0200

    Change RequestMapping to GetMappinb

--- src/main/java/at/technikum/testsiksen/TestsiksenController.java
+++ src/main/java/at/technikum/testsiksen/TestsiksenController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class TestsiksenController {
 
-    @RequestMapping("/sum")
+    @GetMapping("/sum")
     public int sum(@RequestParam int a, @RequestParam int b) {
         return a + b;
     }
